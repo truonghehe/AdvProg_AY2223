7c5cb6caee4341b5255071ee6d327e1731d66a40
Hoàng Thiên Trường <chuongdz@MacBook-Pro-cua-Hoang.local>
2023-04-17 17:47:02 +0700

    fixed

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -37,9 +37,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
-    float rad = (angle / 180) * M_PI;
-    x += cos(rad) * numPixel;
-    y -= sin(rad) * numPixel;
+    jumpForward( -numPixel);
 }
 
 
